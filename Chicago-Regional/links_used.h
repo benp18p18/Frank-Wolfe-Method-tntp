b67298b4962d2c293606e0d1a2ea6b84a569ba3c
benp18p18 <bpollack@vols.utk.edu>
2016-07-26 21:26:43 -0400

    1

--- Chicago-Regional/links_used.h
+++
@@ -1,6 +0,0 @@
-#ifndef LINKS_USED
-#define LINKS_USED
-#include "defines.h"
-
-void links_used(double **links, char ***trips, struct values *data);
-#endif
--- Chicago-Regional/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST
-#define TEST
-
-#include "defines.h"
-
-long double test(double flows[NUM_LINKS], char ***network);
-
-#endif
